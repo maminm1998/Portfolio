6a2e82ff11bf32a48ce49ab897153b2316dc7ebe
Amin <maminm1998@gmail.com>
2023-03-10 09:23:56 +0330

    4

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import Preloader from "../src/components/Pre";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home/Home";
-import About from "./components/About/About";
+
 import Projects from "./components/Projects/Projects";
 import Footer from "./components/Footer";
 import Resume from "./components/Resume/ResumeNew";
@@ -37,7 +37,7 @@ function App() {
         <Routes>
           <Route path="/Portfolio" element={<Home />} />
           <Route path="/Portfolio#/project" element={<Projects />} />
-          <Route path="/Portfolio#/about" element={<About />} />
+          
           <Route path="/Portfolio#/resume" element={<Resume />} />
           <Route path="*" element={<Navigate to="/Portfolio"/>} />
         </Routes>
